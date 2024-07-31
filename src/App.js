de628908d5c90d904e81437c268db42591c14a53
sarathveera44 <sarathveera44@gmail.com>
2024-07-31 20:16:11 +0530

    new router switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import DashboardPanel from './pages/DashboardPanel';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
@@ -8,13 +7,11 @@ import SignIn from './pages/SignIn';
 function App() {
   return (
     <Router>
-      <Fragment>
-        <Routes>
-        <Route path="/signin" element={<SignIn />} />? 
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/dashboard" element={<DashboardPanel />} />
-        </Routes>
-      </Fragment>
+        <Switch>
+        <Route path="/signin" component={<SignIn />} />? 
+        <Route path="/signup" component={<SignUp/>} />
+        <Route path="/dashboard" exact component={<DashboardPanel />} />
+        </Switch>
     </Router>
   );
 }
